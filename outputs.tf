de21cb824f68c3b059a4987f05736bea216ece8d
triet <pvmtriet232@gmail.com>
2023-08-01 09:06:08 +0700

    add

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-/*
-output "instance_ami" {
-  value = aws_instance.ubuntu.ami
+
+output "vpc_id_internet" {
+  value = module.my-internet-vpc.vpc_id
 }
 
-output "instance_arn" {
-  value = aws_instance.ubuntu.arn
+output "vpc_id_intranet" {
+  value = module.my-intranet-vpc.vpc_id
 }
-*/
+
